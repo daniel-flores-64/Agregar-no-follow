fc9a999c70cef965921685356d0c593c7f619e9a
Daniel <Daniel>
2022-12-14 13:18:37 -0600

    03. Se agrego el comando para imprimir los links como un arreglo

--- js/effects.js
+++ js/effects.js
@@ -1,8 +1,13 @@
 function findNReplaceTag(){
     let indexa = /indexa.mx\//g;
     let element = document.getElementsByTagName("link");
+    /*for(let i=0;i<links.length;i++){
+        if(element[i].getAttribute("rel") != "no-follow" && indexa.test(element.getAttribute("href")))
+            element[i].setAttribute("rel", "no-follow");
+    }*///Agregar esta función para cambiar los atributos de las etiquetas
     for(let i=0;i<element.length;i++){
         var gotElem = element[i].outerHTML;
         console.log(gotElem)
     }
+    console.log(element);
   }
