a7d6d2946fc0d33fef02aa5883fb3d255ba7a8a1
Daniel <Daniel>
2022-12-15 12:26:10 -0600

    06. Se agregó documentación del funcionamiento del js

--- js/effects.js
+++ js/effects.js
@@ -1,11 +1,15 @@
 function findNReplaceTag(){
-    let indexa = /https:\/\/indexa.mx\//g;
-    let links = document.getElementsByTagName("link");
+    let indexa = /https:\/\/indexa.mx\//;//Expresión regular para evaluar si la dirección proviene de indexa
+    let links = document.getElementsByTagName("link");//Genera un arreglo que contiene todas las etiquetas link y su contenido
+    let relPrev = "";
     for(let i=0;i<links.length;i++){
-        if(links[i].getAttribute("rel") != "nofollow" && !indexa.test(links[i].getAttribute("href")))
-            links[i].setAttribute("rel", "nofollow");
-        else
-            links[i].setAttribute("rel", "follow");
+		relPrev = "";
+        if(links[i].getAttribute("rel") != "follow" && indexa.test(links[i].getAttribute("href"))){//Si no encuentra la etiqueta follow e identifica la dirección raíz de indexa agrega el valor follow a la etiqueta rel que ya contenía
+            relPrev = links[i].getAttribute("rel");
+            links[i].setAttribute("rel", relPrev+" follow");
+		}else if(links[i].getAttribute("rel") != "nofollow" && !indexa.test(links[i].getAttribute("href"))){//Si no encuentra la etiqueta nofollow e identifica que no contiene la dirección raíz de indexa agrega el valor nofollow a la etiqueta rel que ya contenía
+			relPrev = links[i].getAttribute("rel");
+			links[i].setAttribute("rel", relPrev+" nofollow");
+		}
     }
-    console.log(links);
-  }
+}
